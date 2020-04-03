660975c261dc0b478206c9279e85f58d47f55955
Maksat Zairbekov <maksat.zairbekov@gmail.com>
2020-04-03 23:16:33 +0600

    Updates

---
+++ precache-manifest.845462a30be06b4252f08f93c881eb74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "021aed4ff7e1d31d5766",
+    "url": "/react/static/css/main.f0a78255.chunk.css"
+  },
+  {
+    "revision": "021aed4ff7e1d31d5766",
+    "url": "/react/static/js/main.66de1e7b.chunk.js"
+  },
+  {
+    "revision": "b2ceef7fcbd35532b1b5",
+    "url": "/react/static/js/runtime~main.2e87a795.js"
+  },
+  {
+    "revision": "ac164eeb7fe39897100f",
+    "url": "/react/static/js/2.aa7a0768.chunk.js"
+  },
+  {
+    "revision": "66788b515d23e0c658572be1aed1521a",
+    "url": "/react/index.html"
+  }
+];
